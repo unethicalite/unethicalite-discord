a31fbb4d0d716a3e2dd5471f67a7ccc5ca0bb335
burak <inanb1994@gmail.com>
2022-06-03 16:41:23 +0200

    Use latest version of dto

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("net.dv8tion:JDA:5.0.0-alpha.9")
-    implementation("net.unethicalite:unethicalite-dto:0.0.2")
+    implementation("net.unethicalite:unethicalite-dto:0.0.4")
 }
 
 tasks.withType<KotlinCompile> {
